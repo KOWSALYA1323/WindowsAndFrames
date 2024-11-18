f27f814c3e048e9d6b2b827833b558e05dfaebe9
KOWSALYA1323 <kowsalyavelusamybe@gmail.com>
2024-11-18 23:18:06 +0530

    Windows and Frames task completed...

--- WindowsFrames/src/main/java/windowsframestask/WindowTest.java
+++ WindowsFrames/src/main/java/windowsframestask/WindowTest.java
@@ -1,11 +1,9 @@
 package windowsframestask;
 
 import java.time.Duration;
-import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
